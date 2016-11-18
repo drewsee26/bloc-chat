2fedbe2abf969141aeb94f3b4be9ec3f46791464
Drew Hollenback <drewsee26@gmail.com>
2016-11-17 19:23:24 -0700

    Bloc Chat List Chat Rooms

---
+++ app/scripts/controllers/HomeCtrl.js
@@ -0,0 +1,11 @@
+(function() {
+    function HomeCtrl(Room) {
+        this.title = "Bloc Chat";
+        this.rooms = Room.all;
+        
+    }
+
+    angular
+        .module('blocChat')
+        .controller('HomeCtrl', ['Room', HomeCtrl]);
+})();
